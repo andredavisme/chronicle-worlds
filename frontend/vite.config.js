71268e32796bf1a7fc30bd89e0282e5d30f585c1
andredavisme <andredavisme@users.noreply.github.com>
2026-05-10 23:38:21 +0000

    deploy: fb997a3bf5f723312bdf8bf0c3953712e1279a3c

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig({
-  base: '/chronicle-worlds/', // GitHub Pages serves under repo name
-  build: {
-    outDir: '../docs', // GitHub Pages serves from /docs on main branch
-    emptyOutDir: true,
-  },
-})
